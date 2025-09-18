b36bae6f31ea4ecc80bb286be397c5437f76013e
affragak <affragak@gmail.com>
2025-09-18 16:41:25 +0000

    fix: invalid user choice

--- 100-days-of-python/Day-4/Rock Paper Scissors/solution.py
+++ 100-days-of-python/Day-4/Rock Paper Scissors/solution.py
@@ -31,7 +31,13 @@ game = [rock, paper, scissors]
 user_choice = int(
     input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 )
-print(game[user_choice])
+
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number. You lose!")
+    exit()
+else:
+    print(game[user_choice])
+
 
 computer_choice = random.randint(0, 2)
 print("Computer chose:")
